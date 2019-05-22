331debd03f706220b840c478f496d9ed92bfa1fa
jincheng.qin <jincheng.qin@majorbio.com>
2019-05-22 22:12:19 +0800

    new_code-2to3_play

---
+++ test/argument.py
@@ -0,0 +1,12 @@
+
+class MyClass:
+    def __init__(self):
+        self.a = 'a'
+
+
+var1 = 'miniconda2'
+var2 = 'miniconda3'
+
+if __name__ == '__main__':
+    var1 = 'anaconda2'
+    var3 = 'anaconda3'
---
+++ test/main.py
@@ -0,0 +1,14 @@
+import argument
+
+print(argument.var1)
+
+from argument import MyClass
+
+c = MyClass()
+print(c.a)
+
+from argument import var2
+
+print(var2)
+
+__main__
