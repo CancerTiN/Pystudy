057af4d4fc2895d94ab7031e01629a68001b12a8
jincheng.qin <jincheng.qin@majorbio.com>
2019-10-11 20:50:20 +0800

    new_code-regression_8.2

--- MLiA/chapter08/regression.py
+++ MLiA/chapter08/regression.py
@@ -19,3 +19,29 @@ def standRegres(xArr, yArr):
         return
     ws = xTx.I * (xMat.T * yMat)
     return ws
+
+
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    m = np.shape(xMat)[0]
+    weights = np.mat(np.eye(m))
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = np.exp(diffMat * diffMat.T / (-2 * k ** 2))
+    else:
+        xTx = xMat.T * (weights * xMat)
+    if np.linalg.det(xTx) == 0.0:
+        print('this matrix is singular, cannot do inverse')
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = np.shape(testArr)[0]
+    yHat = np.zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    else:
+        return yHat
--- MLiA/chapter08/run.py
+++ MLiA/chapter08/run.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from MLiA.chapter08 import regression
@@ -16,5 +17,49 @@ def test0():
     print('#' * 64)
 
 
+def test1():
+    print('#' * 64)
+    xArr, yArr = regression.loadDataSet('ex0.txt')
+    ws = regression.standRegres(xArr, yArr)
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+    xCopy = xMat.copy()
+    xCopy.sort(0)
+    yHat = xCopy * ws
+    ax.plot(xCopy[:, 1], yHat)
+    plt.show()
+    print('#' * 64)
+    corrArr = np.corrcoef(yHat.T, yMat)
+    print('corrArr: {}'.format(corrArr))
+    print('#' * 64)
+
+
+def test2():
+    print('#' * 64)
+    xArr, yArr = regression.loadDataSet('ex0.txt')
+    yHat0 = regression.lwlr(xArr[0], xArr, yArr, 1.0)
+    print('x: {}, y: {}, k: {}'.format(xArr[0], yHat0, 1.0))
+    yHat1 = regression.lwlr(xArr[0], xArr, yArr, 0.001)
+    print('x: {}, y: {}, k: {}'.format(xArr[0], yHat1, 0.001))
+    print('#' * 64)
+    for k in [1.0, 0.01, 0.003]:
+        yHat = regression.lwlrTest(xArr, xArr, yArr, k)
+        xMat = np.mat(xArr)
+        yMat = np.mat(yArr)
+        srtInd = xMat[:, 1].argsort(0)
+        xSort = xMat[:, 1].getA1()[srtInd]
+        ySort = yHat[srtInd]
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        plt.title('k = {}'.format(k))
+        ax.plot(xSort, ySort)
+        ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0], s=2, c='red')
+        plt.show()
+    print('#' * 64)
+
+
 if __name__ == '__main__':
-    test0()
+    test2()
